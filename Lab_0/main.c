8738feb2967d74401492cf7e684911d37d0dc972
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2022-01-09 19:13:14 -0800

    Create file

---
+++ Lab_0/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello World\n");
+
+    for(int i = 0; i < argc; i++)
+    {
+        printf("%d - %s\n", i, argv[i]);
+    }
+    return 0;
+}
