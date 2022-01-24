14b6f5b92f3baed8af34427e737a70d4dc2991bf
CrazyCipherGamer5827 <raja.sell5827@gmail.com>
2022-01-23 18:44:50 -0800

    Add all Lab 1 parts

--- Lab_1/main.c
+++ Lab_1/main.c
@@ -1,36 +1,130 @@
 #include<stdio.h>
 
-int main()
+void SizeOfVariables()
 {
-    int num1, num2;  
-    float fraction;     
+    char c;
+    short s;
+    int i;
+    unsigned int ui;
+    unsigned long int ul;
+    float f;
+    double d;
+    long double ld;
+    void *ptr;
+
+    printf("Char c has size: %d\n", sizeof(c));
+    printf("Short s has size: %d\n", sizeof(s));
+    printf("Int i has size: %d\n", sizeof(i));
+    printf("Unsigned int ui has size: %d\n", sizeof(ui));
+    printf("Unsigned long int ul has size: %d\n", sizeof(ul));
+    printf("Float f has size: %d\n", sizeof(f));
+    printf("Double d has size: %d\n", sizeof(d));
+    printf("Long double ld has size: %d\n", sizeof(ld));
+    printf("Pointer ptr has size: %d\n", sizeof(ptr));
+}
+
+int IsLeapYear(int aYear)
+{
+    if(aYear < 1752 && aYear > 0)
+    {
+        printf("The year entered is %d and it is not a leap year.\n", aYear);
+        return 0;
+    }
+    else if(aYear % 400 == 0)
+    {
+        printf("The year entered is %d and it is a leap year.\n", aYear);
+        return 1;
+    }
+    else if(aYear % 100 == 0)
+    {
+        printf("The year entered is %d and it is a not a leap year.\n", aYear);
+        return 0;
+    }
+    else if(aYear % 4 == 0)
+    {
+        printf("The year entered is %d and it is a leap year.\n", aYear);
+        return 1;
+    }
+    else
+    {
+        printf("The year entered is %d and it is not a leap year.\n", aYear);
+        return 0;
+    }
+}
+
+int IsVowel(char aLetter)
+{
+    if ((aLetter >= 0 && aLetter <= 64) || (aLetter >= 91 && aLetter <= 96) || (aLetter >= 123))
+    {
+        printf("Error: Non-alphabetic character\n");
+        return 0;
+    }
+    
+    if(aLetter == 'a' || aLetter == 'e' || aLetter == 'i' || aLetter == 'o' || aLetter == 'u' || aLetter == 'A' || aLetter == 'I' || aLetter == 'E' || aLetter == 'O' || aLetter == 'U')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+void ReverseNumber(long aNumber)
+{
+    short isNegative = 0;
+    long aNumberCopy = aNumber;
+    long reversed_number = 0;
+    if (aNumber < 0)
+    {
+        isNegative = 1;
+        aNumber = -aNumber;
+    }
+    
+    while(aNumber != 0)
+    {
+        reversed_number *= 10;              //These 2 lines of code put the unit's place digit from the original and shift it left in the reversed number.
+        reversed_number += aNumber % 10;
+        aNumber /= 10; //This line removes the unit's place digit we just added onto our reversed number.
+    }
+
+    switch (isNegative)
+    {
+    case 1:
+        printf("Number: %d\tReversed: %d\n", aNumberCopy, -reversed_number);
+        break;
+    
+    default:
+        printf("Number: %d\tReversed: %d\n", aNumberCopy, reversed_number);
+        break;
+    }
+}
+
+int main()
+{    
     char character;
-    char str[50];   // char array of size 50
+    SizeOfVariables();
+    int listOfYears[] = {1750, 1800, 1900, 2000, 1904, 1908, 1996, 2018, 2020, 2024};
 
-    printf("Enter two numbers\n");
+    for(int i = 0; i < sizeof(listOfYears)/sizeof(int); i++)
+    {
+        IsLeapYear(listOfYears[i]);
+    }
+
+    char *testString = "aibIOqE23$%";
+    for (int i = 0; i < 11; i++)
+    {
+        IsVowel(testString[i]);
+    }
+
+    long testNumbers[] = {-234, -100, 0, 123, 34, 234234, 729572};
+    for (int i = 0; i < 7; i++)
+    {
+        ReverseNumber(testNumbers[i]);
+    }
     
-    // Taking integer as input from user
-    scanf(" %d%i", &num1, &num2);
-    printf("\n\nThe two numbers You have entered are %d and %i\n\n", num1, num2);
-
-    // Taking float or fraction as input from the user
-    printf("\n\nEnter a Decimal number\n");
-    scanf(" %f", &fraction); 
-    printf("\n\nThe float or fraction that you have entered is %f", fraction);
-
-    // Taking Character as input from the user
-    printf("\n\nEnter a Character\n");
-    scanf(" %c",&character);
-    printf("\n\nThe character that you have entered is %c and the ASCII number is %d", character, character);
-    getchar();
-
-    printf("\n\nEnter your complete name:\n\n\n");
-
-    gets(str);
-    printf("\n\nWelcome to ECEGR 2020  %s\n\n\n", str);
-
-    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
     
+
     printf("Press Enter to quit");
     scanf("%c",&character);
     return 0;
